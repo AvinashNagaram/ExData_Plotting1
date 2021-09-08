1fcb30c96b7ae357eba4a71703f5a30ec58af703
AvinashNagaram <87401699+AvinashNagaram@users.noreply.github.com>
2021-09-08 00:55:20 -0500

    UPDATE PLOT2

--- plot2.R
+++ plot2.R
@@ -1,1 +1,4 @@
   plot(t$Global_active_power~t$dateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+  dev.copy(png, "C:/Users/DELL/Desktop/RStudio/Projects/Exploratory Analysis/plot2.png", width = 480,  height=480)
+
+dev.off()
