80def2d36615c4ee956bb93938389ed0ca1180a6
AvinashNagaram <87401699+AvinashNagaram@users.noreply.github.com>
2021-09-08 00:54:50 -0500

    UPDATE PLOT1

--- plot1.R
+++ plot1.R
@@ -25,3 +25,8 @@ table<- cbind(dateTime, table)
 table$dateTime <- as.POSIXct(dateTime)
 ##plot 1
 hist(table$Global_active_power, main="Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
+
+
+  dev.copy(png, "C:/Users/DELL/Desktop/RStudio/Projects/Exploratory Analysis/plot1.png", width = 480,  height=480)
+
+dev.off()
