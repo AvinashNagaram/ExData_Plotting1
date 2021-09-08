f406c79b459e5570072aba478fd0b8645350569a
AvinashNagaram <87401699+AvinashNagaram@users.noreply.github.com>
2021-09-08 00:55:45 -0500

    UPDATE PLOT3

--- plot3.R
+++ plot3.R
@@ -7,3 +7,7 @@
   })
   legend("topright", col=c("black", "red", "blue"), lwd=c(1,1,1), 
          c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+  dev.copy(png, "C:/Users/DELL/Desktop/RStudio/Projects/Exploratory Analysis/plot3.png", width = 480,  height=480)
+
+dev.off()
